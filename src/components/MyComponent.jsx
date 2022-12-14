26b785067a59e516b6a3d65da9f236b5697f502a
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-15 01:03:48 +0700

    form react #21

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -12,7 +12,7 @@ export default class MyComponent extends Component {
             age: "25",
             address: "Hoai Duc",
         })
-       console.log(event.target)
+        console.log(event.target)
     }
     handleOnChange = (event) => {
         this.setState({
@@ -26,19 +26,39 @@ export default class MyComponent extends Component {
             age: Math.floor(Math.random() * 100) + 1,
         })
     }
-  render() {
-    return (
-        <>
-            <div>MyComponent</div>
-            <div>Name: {this.state.name}</div>
-            <div>Age: {this.state.age}</div>
-            <div>Address: {this.state.address}</div>
-            
-            <input type="text" value={this.state.name} onChange={(event) => this.handleOnChange(event)} />
-            <button className='btn btn-danger' onClick={(event) => this.handleClick(event)}>Click me!</button>
-            <button className='btn btn-danger' onMouseOver={(event) => this.handleOnMouseOver(event)}>Hover me!</button>
+    handleOnChangeInput = (event) => {
+        this.setState({
+            address: event.target.value
+        })
+    }
 
-        </>
-    )
-  }
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log("state: ", this.state);
+    }
+    render() {
+        return (
+            <>
+                <div className='react-basic'>
+                    <div>MyComponent</div>
+                    <div>Name: {this.state.name}</div>
+                    <div>Age: {this.state.age}</div>
+                    <div>Address: {this.state.address}</div>
+
+                    <input type="text" value={this.state.name} onChange={(event) => this.handleOnChange(event)} />
+                    <button className='btn btn-danger' onClick={(event) => this.handleClick(event)}>Click me!</button>
+                    <button className='btn btn-danger' onMouseOver={(event) => this.handleOnMouseOver(event)}>Hover me!</button>
+                </div>
+                <div className='form-react mt-3'>
+                    <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                        <div className='form-group'>
+                            <input type="text" className='form-control' onChange={(event) => this.handleOnChangeInput(event)}/>
+                            <button className='btn btn-warning'>Submit</button>
+                        </div>
+                    </form>
+                </div>
+
+            </>
+        )
+    }
 }
