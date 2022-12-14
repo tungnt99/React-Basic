26cde86b7b3b086d46ab0e283ed928aee6fae0c1
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-15 00:29:15 +0700

    DOM event basic #19

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -6,6 +6,22 @@ export default class MyComponent extends Component {
         age: "24",
         address: "HN"
     }
+    handleClick = (event) => {
+        this.setState({
+            name: "Nguyen Thanh Tung",
+            age: "25",
+            address: "Hoai Duc",
+        })
+       console.log(event.target)
+    }
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+    handleOnMouseOver = (event) => {
+        console.log(event.target);
+    }
   render() {
     return (
         <>
@@ -13,6 +29,11 @@ export default class MyComponent extends Component {
             <div>Name: {this.state.name}</div>
             <div>Age: {this.state.age}</div>
             <div>Address: {this.state.address}</div>
+            
+            <input type="text" value={this.state.name} onChange={(event) => this.handleOnChange(event)} />
+            <button className='btn btn-danger' onClick={(event) => this.handleClick(event)}>Click me!</button>
+            <button className='btn btn-danger' onMouseOver={(event) => this.handleOnMouseOver(event)}>Hover me!</button>
+
         </>
     )
   }
