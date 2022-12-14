569342e516a88f4c80c0952bc05e4c1bc3134e1f
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-14 23:45:54 +0700

    react basic components #16

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class MyComponent extends Component {
+  render() {
+    return (
+      <div>MyComponent {Math.random()}</div>
+    )
+  }
+}
