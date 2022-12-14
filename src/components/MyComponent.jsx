b3177c960b85629b8efa817dbbeec2dd11dce8fd
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-15 00:00:33 +0700

    react basic State #17

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 
 export default class MyComponent extends Component {
+    state = {
+        name: 'Thanh Tung',
+        age: "24",
+        address: "HN"
+    }
   render() {
     return (
-      <div>MyComponent {Math.random()}</div>
+        <>
+            <div>MyComponent {Math.random()}</div>
+            <div>Name: {this.state.name}</div>
+            <div>Age: {this.state.age}</div>
+            <div>Address: {this.state.address}</div>
+        </>
     )
   }
 }
