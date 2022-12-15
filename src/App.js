7ea926dc62b6ceca7c151cf4401c90099b7631dc
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-16 00:41:03 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+
 import MyComponent from './components/MyComponent';
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
 
-//   return (
-//     <>
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-
-//           <div>Count = {count}</div>
-//           <button className='btn btn-warning' onClick={() => dispatch(increaseCounter())}>Increase</button>
-//           <button className='btn btn-success' onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//           <MyComponent />
-//         </header>
-//       </div>
-//     </>
-//   );
-// }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
           <MyComponent />
         </header>
       </div>
