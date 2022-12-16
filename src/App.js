0fcc6529411fabee2ae0e6d1312bdc2e0b98db96
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-17 00:00:14 +0700

    Lifecycle basic #33

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MyComponent from './components/MyComponent';
 
 
 class App extends Component {
+ 
   render() {
     return (
       <div className="App">
--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
 
 export default class DisplayInfo extends Component {
+  constructor(props) {
+    console.log('constructor: 1');
+    super(props);
+    this.state = {
+
+    };
+  }
+  componentDidMount(){
+    console.log('>>>componentDidMount');
+    setTimeout(() => {
+      document.title='Test Lifecycle'
+    }, 2000)
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('>>>componentDidUpdate', this.props, prevProps);
+    if(this.props.listUsers !== prevProps.listUsers){
+      if(this.props.listUsers.length === 5){
+        alert('you got 5 user')
+      }
+    }
+  }
   render() {
     // props
+    console.log('render');
+
     let {listUsers} = this.props
     return (
       <div>
