65ea1937e7b476c6bec72a00a8fad36820fbf9e1
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-17 23:45:48 +0700

    state hóa modal create user & upload file image #57

--- src/components/Admin/Contents/ManageUser/ManageUsers.jsx
+++ src/components/Admin/Contents/ManageUser/ManageUsers.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ModalCreateUser from './ModalCreateUser'
+import './assets/manage.scss';
+import { FcPlus } from "react-icons/fc";
 
 export default function ManageUsers(props) {
+  const [showModalCreateUser, setShowModalCreateUser] = useState(false);
   return (
-    <div className="manage-user-container">
-    <div className="title">
-      Manage User
-    </div>
-    <div className="user-content">
-      {/* <div>
-        <button className='btn btn-success'>Add new users</button>
-      </div> */}
-      <div>
-        <ModalCreateUser />
+    <div className="manage-user-container container">
+      <div className="title">
+        Manage User
+      </div>
+      <div className="manage-user-content">
+        <div className='btn-add-user'>
+          <button className='btn btn-success' onClick={() => setShowModalCreateUser(true)}><FcPlus /> Add new users</button>
+        </div>
+        <div className='create-user-content'>
+          <ModalCreateUser
+            show={showModalCreateUser}
+            setShow={setShowModalCreateUser}
+          />
+        </div>
       </div>
     </div>
-  </div>
   )
 }
--- src/components/Admin/Contents/ManageUser/ModalCreateUser.jsx
+++ src/components/Admin/Contents/ManageUser/ModalCreateUser.jsx
@@ -3,18 +3,34 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from "react-icons/fc";
 
-export default function ModalCreateUser() {
-    const [show, setShow] = useState(false);
+export default function ModalCreateUser(props) {
+    const {show, setShow} = props;
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
 
+    // state
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userName, setUserName] = useState('');
+    const [role, setRole] = useState('USER');
+    const [image, setImage] = useState('');
+    const [previewImage, setPreviewImage] = useState("");
+
+    // preview image
+    const handleUploadImage = (event) => {
+        if (event.target && event.target.files && event.target.files[0]) {
+            setPreviewImage(URL.createObjectURL(event.target.files[0]))
+            setImage(event.target.files[0])
+            
+        } else {
+            // setPreviewImage("");
+        }
+        console.log("upload file", event.target.files[0]);
+    }
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Add new user
-            </Button>
+          
 
-            <Modal show={show} onHide={handleClose} size="xl" backdrop="static">
+            <Modal show={show} onHide={handleClose} size="xl" backdrop="static" className="modal-add-user">
                 <Modal.Header closeButton>
                     <Modal.Title>Add New User</Modal.Title>
                 </Modal.Header>
@@ -22,27 +38,35 @@ export default function ModalCreateUser() {
                     <form className="row">
                         <div className="col-md-6 mb-3">
                             <label className="form-label">Email</label>
-                            <input type="email" className="form-control" />
+                            <input type="email" className="form-control" value={email} onChange={(event) => setEmail(event.target.value)} />
                         </div>
                         <div className="col-md-6 mb-3">
                             <label className="form-label">Password</label>
-                            <input type="password" className="form-control" />
+                            <input type="password" className="form-control" value={password} onChange={(event) => setPassword(event.target.value)} />
                         </div>
 
                         <div className="col-md-6 mb-3">
                             <label className="form-label">Username</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" value={userName} onChange={(event) => setUserName(event.target.value)} />
                         </div>
                         <div className="col-md-6 mb-3">
                             <label className="form-label">Role</label>
-                            <select className="form-select">
-                                <option selected value="USER">USER</option>
+                            <select className="form-select" value={role} onChange={(event) => setRole(event.target.value)}>
+                                <option value="USER">USER</option>
                                 <option value="ADMIN">ADMIN</option>
                             </select>
                         </div>
                         <div className="col-md-12">
-                            <label className="form-label" htmlFor='upload-image' role="button"> <FcPlus /> Upload File Image</label>
-                            <input type="file" className="form-control" id="upload-image" hidden />
+                            <label className="form-label label-upload" htmlFor='upload-image' role="button"> <FcPlus /> Upload File Image</label>
+                            <input type="file" className="form-control" id="upload-image" hidden onChange={(event) => handleUploadImage(event)} />
+                        </div>
+                        <div className='col-md-12 img-preview'>
+                            {previewImage ?
+                                <img src={previewImage} alt={image} />
+                                :
+                                <span>Preview Image</span>
+
+                            }
                         </div>
                     </form>
                 </Modal.Body>
