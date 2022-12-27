a2b38c926a1d486343ca4702b1ba757d51db9487
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-27 23:02:04 +0700

    Process data - dùng lodash xử lý data phía frontend #83

--- src/components/User/DetailQuiz.jsx
+++ src/components/User/DetailQuiz.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getDataQuestions } from '../../services/apiServices';
+import _ from 'lodash';
 
 export default function DetailQuiz(props) {
     const [title] = useState('Test Quiz User')
@@ -11,14 +12,42 @@ export default function DetailQuiz(props) {
     useEffect(() => {
         document.title = title;
         fetchDataQuestion();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [title, quizId])
 
-    const fetchDataQuestion = async() => {
-        let data = await getDataQuestions(quizId);
-        console.log('data: ', data);
+    const fetchDataQuestion = async () => {
+        let res = await getDataQuestions(quizId);
+        // console.log('res: ', res);
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            // console.log('raw: ', raw);
+            let data = _.chain(raw)
+                    // Group the elements of Array based on `color` property
+                    .groupBy("id")
+                    // `key` is group's name (color), `value` is the array of objects
+                    .map((value, key) => {
+                          // console.log('value', value, 'key', key);
+                        let answers = [];
+                        let questionDescription, image = null;
+                        // answers.questionId = key;
+                        value.forEach((item, index) => {
+                            if(index === 0){
+                                questionDescription = item.description;
+                                image = item.image;
+                            }
+                            // console.log('item: ', item);
+                            // console.log('items answer', item.answers);
+                            answers.push(item.answers);
+                        }) 
+                      
+                        return { questionId: key, answers, questionDescription, image }
+                    })
+                    .value();
+            // console.log("data: ", data);
+            
+        }
     }
-  return (
-    <div>DetailQuiz</div>
-  )
+    return (
+        <div>DetailQuiz</div>
+    )
 }
