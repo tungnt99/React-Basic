9243114688a7ff0f1ed366b2bbd07443ad2cc6bc
Tùng <92722248+tungnt99@users.noreply.github.com>
2023-01-02 15:06:53 +0700

    state hóa data #96

--- src/components/Admin/Contents/Question/Questions.jsx
+++ src/components/Admin/Contents/Question/Questions.jsx
@@ -53,37 +53,88 @@ export default function Questions() {
             setQuestions([...questions, newQuestion]);
         }
         if (type === "REMOVE") {
-            let questionClone = _.cloneDeep(questions);
-            questionClone = questionClone.filter((item) => item.id !== id);
-            setQuestions(questionClone);
+            let questionsClone = _.cloneDeep(questions);
+            questionsClone = questionsClone.filter((item) => item.id !== id);
+            setQuestions(questionsClone);
         }
     }
 
     const handleAddRemoveAnswer = (type, questionId, answerId) => {
         // console.log(type, questionId, answerId);
-        let questionClone = _.cloneDeep(questions); // clone lại dữ liệu trong mảng questions ban đầu
+        let questionsClone = _.cloneDeep(questions); // clone lại dữ liệu trong mảng questions ban đầu
         if (type === 'ADD') {
             const newAnswer = {
                 id: uuidv4(),
                 description: "",
                 isCorrect: false,
             };
-            let index = questionClone.findIndex((item) => item.id === questionId);
+            let index = questionsClone.findIndex((item) => item.id === questionId);
             // console.log(index);
-            questionClone[index].answers.push(newAnswer); //cập nhật mảng newAnswer mới vào mảng ban đầu
+            questionsClone[index].answers.push(newAnswer); //cập nhật mảng newAnswer mới vào mảng ban đầu
 
             // console.log("index", index);
-            setQuestions(questionClone);
+            setQuestions(questionsClone);
         }
         if (type === "REMOVE") {
-            let index = questionClone.findIndex((item) => item.id === questionId);
+            let index = questionsClone.findIndex((item) => item.id === questionId);
 
-            questionClone[index].answers = questionClone[index].answers.filter(
+            questionsClone[index].answers = questionsClone[index].answers.filter(
                 (item) => item.id !== answerId
             );
 
-            setQuestions(questionClone);
+            setQuestions(questionsClone);
         } // remove old questions
+    }
+
+    const handleOnChangeQuestion = (type, questionId, value) => {
+        // console.log('type: ', type, 'questionId: ', questionId, 'value: ', value);
+        if (type === 'QUESTION') {
+            let questionsClone = _.cloneDeep(questions); // clone lại dữ liệu trong mảng questions ban đầu
+            let index = questionsClone.findIndex((item) => item.id === questionId);
+            if (index > -1) {
+                questionsClone[index].description = value;
+                setQuestions(questionsClone);
+            }
+        }
+    }
+
+    const handleOnChangeFileQuestion = (questionId, event) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex((item) => item.id === questionId);
+        if (index > -1 && event.target && event.target.files && event.target.files[0]) {
+            questionsClone[index].imageFile = event.target.files[0];
+            // console.log("check file", event.target.files[0]);
+            questionsClone[index].imageName = event.target.files[0].name;
+
+            setQuestions(questionsClone)
+        }
+    }
+
+    const handleAnswerQuestion = (type, answerId, questionId, value) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+        // console.log(type, questionId, answerId, value, index);
+        if (index > -1) {
+            questionsClone[index].answers = questionsClone[index].answers.map(answer => {
+                // console.log('answer: ', answer);
+                if (answer.id === answerId) {
+                    if (type === 'CHECKBOX') {
+                        answer.isCorrect = value;
+                    }
+
+                    if (type === 'INPUT') {
+                        answer.description = value;
+                    }
+                }
+                return answer;
+            })
+            //    console.log('answer', questionsClone);
+            setQuestions(questionsClone)
+        }
+    }
+
+    const handleSubmitQuestionForQuiz = () => {
+        console.log('submitQuestion', questions);
     }
     return (
         <div className="questions-container container">
@@ -104,13 +155,13 @@ export default function Questions() {
                             <div className="question-main my-3" key={question.id}>
                                 <div className="row question-content">
                                     <div className="form-floating mt-3 col-6">
-                                        <input type="text" className="form-control" placeholder="Question: {index + 1}" defaultValue={question.description} />
+                                        <input type="text" className="form-control" placeholder="Question: {index + 1}" defaultValue={question.description} onChange={(event) => handleOnChangeQuestion("QUESTION", question.id, event.target.value)} />
                                         <label>Question: {index + 1}</label>
                                     </div>
-                                    <div className="col-4 px-3">
-                                        <label className="">Upload Image</label>
-                                        <input type="file" className="form-control" />
-                                        <span>0 file is uploaded</span>
+                                    <div className="col-2 px-3 mt-3 d-flex flex-column align-items-center">
+                                        <label htmlFor={`${question.id}`} role="button" className="btn btn-outline-secondary ">Upload Image</label>
+                                        <input id={`${question.id}`} hidden type="file" className="form-control" onChange={(event) => handleOnChangeFileQuestion(question.id, event)} />
+                                        <span>{question.imageName ? question.imageName : "0 file is uploaded"}</span>
                                     </div>
 
                                     <div className="col-2 d-flex align-items-center gap-2">
@@ -128,9 +179,9 @@ export default function Questions() {
                                     {question.answers && question.answers.length > 0 && question.answers.map((answer, index) => {
                                         return (
                                             <div className="d-flex align-items-center flex-start mb-3" key={answer.id}>
-                                                <input type="checkbox" className="form-check-input" />
+                                                <input type="checkbox" className="form-check-input" checked={answer.isCorrect} onChange={(event) => handleAnswerQuestion("CHECKBOX", answer.id, question.id, event.target.checked)} />
                                                 <div className="form-floating mx-3 col-6">
-                                                    <input type="text" className="form-control" placeholder="Answer {index + 1}" defaultValue={answer.description} />
+                                                    <input type="text" className="form-control" placeholder="Answer {index + 1}" defaultValue={answer.description} onChange={(event) => handleAnswerQuestion("INPUT", answer.id, question.id, event.target.value)} />
                                                     <label>Answer: {index + 1}</label>
                                                 </div>
                                                 <div className="col-3 d-flex align-items-center gap-2">
@@ -150,6 +201,11 @@ export default function Questions() {
                             </div>
                         )
                     })}
+                    {questions && questions.length > 0 &&
+                        <div>
+                            <button onClick={() => handleSubmitQuestionForQuiz()} className="btn btn-warning">Save Questions</button>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
