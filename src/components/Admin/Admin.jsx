c3d345639b3ae87f98ded72fb8318654cb024fce
Tùng <92722248+tungnt99@users.noreply.github.com>
2023-01-10 10:32:04 +0700

    design dashboard #110

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -5,9 +5,15 @@ import './assets/admin.scss'
 import { Outlet } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import PerfectScrollbar from 'react-perfect-scrollbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useTranslation } from 'react-i18next';
+import Language from '../Header/Language';
 
 
 export default function Admin(props) {
+  // translation
+  const { t } = useTranslation();
+  // end translation
   const [collapsed, setCollapsed] = useState(false);
   const [title] = useState('Admin');
 
@@ -22,6 +28,13 @@ export default function Admin(props) {
       <div className="admin-content" role="button">
         <div className='admin-header'>
           <FaBars onClick={() => setCollapsed(!collapsed)} />
+          <div className='d-flex align-items-center gap-3'>
+            <Language />
+            <NavDropdown title={t('header.setting')} id="basic-nav-dropdown-admin">
+              <NavDropdown.Item className='nav-link px-3' to="#action/3.3">{t('header.profile')}</NavDropdown.Item>
+              <NavDropdown.Item className='nav-link px-3'>{t('header.logout')}</NavDropdown.Item>
+            </NavDropdown>
+          </div>
         </div>
         <div className='admin-main'>
           <PerfectScrollbar>
--- src/components/Admin/Contents/Dashboard/Dashboard.jsx
+++ src/components/Admin/Contents/Dashboard/Dashboard.jsx
@@ -1,11 +1,85 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useState, useEffect } from 'react';
+import '../../assets/admin.scss';
+import { BarChart, Legend, Bar, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+const data = [
+  {
+    "name": "Page A",
+    "uv": 4000,
+    "pv": 2400
+  },
+  {
+    "name": "Page B",
+    "uv": 3000,
+    "pv": 1398
+  },
+  {
+    "name": "Page C",
+    "uv": 2000,
+    "pv": 9800
+  },
+  {
+    "name": "Page D",
+    "uv": 2780,
+    "pv": 3908
+  },
+  {
+    "name": "Page E",
+    "uv": 1890,
+    "pv": 4800
+  },
+  {
+    "name": "Page F",
+    "uv": 2390,
+    "pv": 3800
+  },
+  {
+    "name": "Page G",
+    "uv": 3490,
+    "pv": 4300
+  }
+]
 export default function DashBoard(props) {
   const [title] = useState('Dashboard');
   useEffect(() => {
     document.title = title;
   })
   return (
-    <div>DashBoard</div>
+    <div className='dashboard-container container'>
+      <div className='dashboard-title'>
+        Analytics DashBoard
+      </div>
+      <div className='dashboard-content row'>
+        <div className='dashboard-content-left col-md-5'>
+          <div className='dashboard-content-left-total'>
+            <div className='child col-6'>
+              <div className='child-item'>Total users</div>
+            </div>
+            <div className='child col-6'>
+              <div className='child-item'>Total Quizzes</div>
+            </div>
+            <div className='child col-6'>
+              <div className='child-item'>Total Questions</div>
+            </div>
+            <div className='child col-6'>
+              <div className='child-item'>Total Answers</div>
+            </div>
+          </div>
+        </div>
+        <div className='dashboard-content-right col-md-7'>
+          <div className='dashboard-content-right-total'>
+            <BarChart width={730} height={350} data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="pv" fill="#8884d8" />
+              <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
